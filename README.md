772c4818d21d1a0933ad280b4bfe2e37f31cf2c2
Francisco Javier Llanquipichun Garcia <francisco.llanquipichun@gmail.com>
2021-01-05 00:14:16 -0300

    Update symbolic links install process
    Signed-off-by: Francisco Javier Llanquipichun Garcia <francisco.llanquipichun@gmail.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+## install
+
+Clone this repository in your home `~/` directory:
+
+```bash
+git clone https://github.com/franciscollanquipichun/dotfiles.git ~/.dotfiles
+```
+
+run the installation file:
+
+```bash
+cd ~/.dotfiles
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -4,28 +4,30 @@
 # This script run the setup installation
 ############################
 
-########## Variables
+# Variables
 
-dir=~/dotfiles                    # dotfiles directory
+basepath=~/dotfiles               # dotfiles directory
 olddir=~/.dotfiles_bkp            # old dotfiles backup directory
-configs="git vim zsh bash"        # list of files/folders to symlink in homedir
-
-##########
+linkspath=$basepath/links         # symbolic links directories
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
+echo "`tput setaf 2`done`tput sgr0`"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+echo "Changing to the symbolic links directory: $linkspath"
+cd $linkspath
+echo "`tput setaf 2`done`tput sgr0`"
 
-export dir
-export olddir
-
-for config in $configs; do
-    export config
-    ./$config/install.sh
+for dir in `ls -d */ | cut -f1 -d'/'`; do
+    cd $dir
+    for file in `ls`; do
+        echo "Moving .$file from ~ to $olddir"
+        mv ~/.$file $olddir/
+        echo "Creating symlink to `tput bold`$linkspath/$dir/$file`tput sgr0` in home directory."
+        ln -s $linkspath/$dir/$file ~/.$file
+    done
+    cd $linkspath
 done
+echo "`tput setaf 2`done`tput sgr0`"
